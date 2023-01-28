4d76d7ad9fa5b1b2b49fb91a254665e5459251e7
rajmoham <rajmoham10@gmail.com>
2023-01-28 21:45:28 +0000

    Error Page

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -2,7 +2,15 @@ import React from 'react'
 
 const Error = () => {
     return (
-        <div>Error</div>
+        <div className='w-screen h-screen flex justify-center items-center'>
+            <div className='flex flex-col text-center'>
+                <p className='text-3xl font-bold'>
+                    Uh oh! This URL is invalid :(</p>
+                <p className='font-semibold mt-4'>
+                    <a href='/' className='text-blue-600 underline'>Click here </a> 
+                    to return to the main page</p>    
+            </div>
+        </div>
     )
 }
 
